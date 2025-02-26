1914dbdc8a0a36d6f9d6fa8f2169f867b48e98f9
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-02-26 12:56:34 +0600

    So

---
+++ Rom.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# repo init rom
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b QPR3 -g default,-mips,-darwin,-notdefault
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/Gtajisan/local_manifests_clo -b infinity .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# build
+/opt/crave/resync.sh
+echo "============="
+echo "Sync success"
+echo "============="
+
+# remove face unlock 
+rm -rf packages/apps/FaceUnlock
+
+# Export
+export BUILD_USERNAME=FARHAN
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export SELINUX_IGNORE_NEVERALLOWS=true
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Lunch
+. build/envsetup.sh
+ccache -M 50G -F 0
+lunch treble_arm64_bgN-userdebug 
+make systemimage -j$(nproc --all)
+# son
+ cd out/target/product/tdgsi_arm64_ab
+ xz -z -k system.img 
